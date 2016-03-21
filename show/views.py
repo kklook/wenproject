ba6e4ce16e24827a2a4695a6533efebe44c96093
null122 <714779883@qq.com>
2016-03-21 11:30:04 +0800

    beta 1.01

--- show/views.py
+++ show/views.py
@@ -12,7 +12,7 @@ def get(request):
     page=cookie.open()
     if(page==-1):
         return render(request,'home.html',{'userkown':'用户名或密码错误'})
-    m=re.compile(r'body>(.*?)</body>',re.S)
+    m=re.compile(r'<table name="" id="1" class="CourseFormTable" style="" >(.*?)</body>',re.S)
     body=re.search(m,page)
     return render(request,'show.html',{'main':body.group(1)})
 # Create your views here.
