cda74853c852b3bb4fdd5dc4daec5970b5e0ca83
null122 <714779883@qq.com>
2016-03-22 17:31:49 +0800

    beta 1.02

--- dweb/wsgi.py
+++ dweb/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dweb.settings")
 
 application = get_wsgi_application()
